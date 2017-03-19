e69d9f9dc9012b649e880a0b56196b51e35718bc
Venkateshwar Rao Gora <venky586@yahoo.co.in>
2017-03-19 18:09:55 +0530

    debug statement changed

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y(#4--hghwv(5&xwa)01l1rju39w6wr*qr-^ds%nkmd3-b#o$1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
